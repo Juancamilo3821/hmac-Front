6d2c5c9d111441ff50a7cef947a92417cec584e6
jreyess11 <josegabrielreyessolanojr@gmail.com>
2023-10-03 15:13:51 -0500

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-import Vue from "vue";
-import Router from "vue-router";
-
-Vue.use(Router);
-
 const routes = [
     {
       path: '/',
